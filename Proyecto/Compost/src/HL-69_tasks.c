a8da96002669bfb5049bacd2694c714755df7c1a
hugomez17 <38087861+hugomez17@users.noreply.github.com>
2018-12-05 10:09:19 -0300

    Actualizacion HL-69

--- Proyecto/Compost/src/HL-69_tasks.c
+++ Proyecto/Compost/src/HL-69_tasks.c
@@ -21,23 +21,24 @@ extern xQueueHandle qHumedad;
 /* Número de items que la cola puede almacenar */
 //#define mainQUEUE_LENGTH	10
 
-void InitADC(void)
+void initADC(void)
 {
 	ADC_CLOCK_SETUP_T adc;
 	Chip_ADC_Init(LPC_ADC, &adc);
 	Chip_ADC_SetSampleRate(LPC_ADC, &adc, fs);
-	Chip_ADC_EnableChannel(LPC_ADC, ADC_CH1, ENABLE);
-	Chip_ADC_Int_SetChannelCmd(LPC_ADC, ADC_CH1, ENABLE);
+	Chip_ADC_EnableChannel(LPC_ADC, ADC_CH_HUM, ENABLE);
+	Chip_ADC_Int_SetChannelCmd(LPC_ADC, ADC_CH_HUM, ENABLE);
 	Chip_ADC_SetBurstCmd(LPC_ADC, ENABLE);
 	NVIC_EnableIRQ(ADC_IRQn);
 	NVIC_SetPriority(ADC_IRQn,configLIBRARY_MAX_SYSCALL_INTERRUPT_PRIORITY+1);
 
 }
-
+/*
 void ADC_PIN_init(void)
 {
 	Chip_IOCON_PinMuxSet(LPC_IOCON,PIN_ADC1,FUNC_ADC);
 }
+*/
 
 uint16_t calcularPromedio(uint16_t *vector, int cantidad){
 
@@ -57,15 +58,21 @@ void ADC_IRQHandler(void)
 	portBASE_TYPE HigherPriorityTaskWoken = 0;
 	uint16_t data;
 	uint16_t porcentaje;
-	Chip_ADC_ReadValue(LPC_ADC, ADC_CH1, &data);
+	Chip_ADC_ReadValue(LPC_ADC, ADC_CH_HUM, &data);
 
-	porcentaje=100-((data*100)/4095);
-	//xQueueSendFromISR(xQueueADC,&data, &HigherPriorityTaskWoken); 	//Pruebo mandando el dato crudo o el porcentaje
-	xQueueSendFromISR(xQueueADC,porcentaje, &HigherPriorityTaskWoken);
+	xQueueSendFromISR(xQueueADC, &data, &HigherPriorityTaskWoken);
 
 	portEND_SWITCHING_ISR(HigherPriorityTaskWoken);
-	//Chip_ADC_Int_SetChannelCmd(LPC_ADC, ADC_CH1, DISABLE);
+	//Chip_ADC_Int_SetChannelCmd(LPC_ADC, ADC_CH_HUM, DISABLE);
 
+}
+
+uint16_t ReadDataADC()
+{
+	uint16_t dataHumedad;
+	xQueueReceive(xQueueADC, &dataHumedad, portMAX_DELAY);
+	dataHumedad=100-((dataHumedad*100)/4095);
+	return dataHumedad;
 }
 
 void vReadDataADC(void *a)
@@ -109,11 +116,5 @@ void vReadDataADC(void *a)
 	}
 }
 
-uint16_t ReadDataADC()
-{
-	uint16_t porcHumedad;
-	xQueueReceive(xQueueADC, &porcHumedad, portMAX_DELAY);
-	return porcHumedad;
-}
 
 
